531f7abea9fa380807ef842979683090d892ccfb
Pranay <pranaym189@gmail.com>
2022-09-08 22:09:49 +0530

    By IIT Kanpur

---
+++ js/resp.js
@@ -0,0 +1,9 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.rightnav')
+burger.addEventListener('click',()=>{
+
+    navbar.classList.toggle('v-resp-js')
+
+
+})
